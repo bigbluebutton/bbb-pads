3033714b04b46b93284c3f1dcd28627839dcef83
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2021-10-21 07:55:42 -0300

    feat(monitor): data's memory usage

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const api = require('./lib/etherpad/api');
 const subscriber = require('./lib/redis/subscriber');
+const monitor = require('./lib/utils/monitor');
 const Logger = require('./lib/utils/logger');
 
 const logger = new Logger('bbb-pads');
 
 api.call('checkToken').then(response => {
   subscriber.start();
+  monitor.start();
 }).catch(() => {
   logger.error('apikey', 'mismatch');
 });
