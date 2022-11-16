7e82aced0160540c227a9947430c2ef5a11c78a6
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-11-16 13:44:50 +0000

    refactor(config): fix identation in config/index.js

--- config/index.js
+++ config/index.js
@@ -5,22 +5,21 @@ const DEFAULT_SETTINGS_FILE_PATH = './settings.json';
 const LOCAL_SETTINGS_FILE_PATH = '/etc/bigbluebutton/bbb-pads.json';
 
 const localSettingsExists = () => {
-	try {
-		fs.accessSync(LOCAL_SETTINGS_FILE_PATH);
-	} catch (err) {
-		return false;
-	}
-	return true;
+  try {
+    fs.accessSync(LOCAL_SETTINGS_FILE_PATH);
+  } catch (err) {
+    return false;
+  }
+  return true;
 };
 
 const SETTINGS = require(DEFAULT_SETTINGS_FILE_PATH);
 
 if (localSettingsExists()) {
-    const LOCAL_SETTINGS = require(LOCAL_SETTINGS_FILE_PATH);
-    _.mergeWith(SETTINGS, LOCAL_SETTINGS, (a, b) => (_.isArray(b) ? b : undefined));
+  const LOCAL_SETTINGS = require(LOCAL_SETTINGS_FILE_PATH);
+  _.mergeWith(SETTINGS, LOCAL_SETTINGS, (a, b) => (_.isArray(b) ? b : undefined));
 }
 
 const config = SETTINGS;
 
 module.exports = config;
-
